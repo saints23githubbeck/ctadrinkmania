83efaa0961a4c1ff0f0161428252ad4d063d1e74
arthurshadrack <arthurshadrack45@gmail.com>
2022-01-13 16:12:49 +0000

    first commit

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import DashboardRoot from '../DashboardRoot/DashboardRoot';
+import DashboardSlide from '../DashboardSlide/DashboardSlide';
+import ManageAds from '../ManageAds/ManageAds';
+import MyRecipes from '../MyRecipes/MyRecipes';
+import NewsLetters from '../Newsletters/NewsLetters';
+import BlogList from '../Dashboard/BlogList/BlogList';
+
+import Profile from '../Profile/Profile';
+import User from "./User/User";
+
+const Dashboard = () => {
+    return (
+        <div>
+        
+            <DashboardSlide>
+                
+            </DashboardSlide>
+            <NewsLetters></NewsLetters>
+            <ManageAds></ManageAds>
+            <DashboardRoot></DashboardRoot>
+            <Profile></Profile> 
+            <MyRecipes></MyRecipes>
+            <User></User>
+            <BlogList></BlogList>
+        </div>
+    )
+}
+export default Dashboard;
+
